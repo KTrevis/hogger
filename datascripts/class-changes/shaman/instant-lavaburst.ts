fad2bb7feda34387d5c03b2a39630a1d0a2dc023
KTrevis <treviskevin77@gmail.com>
2025-08-19 22:01:04 +0200

    feat(instant-lavaburst-aura)
    - Updated Mathias Barrelbeard's role to "Herbalism Trainer" and adjusted
    NPC flags accordingly.
    - Modified the Herb Collection quest to require 3 items instead of 5 and
    increased the minimum level requirement from 4 to 5.

---
+++ datascripts/class-changes/shaman/instant-lavaburst.ts
@@ -0,0 +1,35 @@
+import { std } from "wow/wotlk";
+import { Spell } from "wow/wotlk/std/Spell/Spell";
+import { MODULE_NAME } from "../../utils/constants";
+import { translate } from "../../utils/translation";
+
+const LAVA_BURST = std.Spells.load(51505);
+
+const INSTANT_LAVABURST = std.Spells.create(
+  MODULE_NAME,
+  "instant-lava-burst-aura",
+  57761
+);
+
+INSTANT_LAVABURST.Name.enGB
+  .set("Instant Lava Burst")
+  .Duration.setSimple(8 * 1000)
+  .Icon.set(LAVA_BURST.Icon.get());
+
+INSTANT_LAVABURST.Family.set(LAVA_BURST.Family.get());
+INSTANT_LAVABURST.Effects.get(0).ClassMask.A.set(LAVA_BURST.ClassMask.A.get());
+INSTANT_LAVABURST.Effects.get(1).ClassMask.A.set(LAVA_BURST.ClassMask.A.get());
+
+namespace Translation {
+  export function english(spell: Spell) {
+    const TOOLTIP = "Your Lava Burst is instant and does not cost mana.";
+    spell.AuraDescription.enGB.set(TOOLTIP);
+    spell.Description.enGB.set(TOOLTIP);
+  }
+}
+
+translate(INSTANT_LAVABURST, {
+  enGB: Translation.english,
+});
+
+const SHAMAN = std.Classes.load("SHAMAN");
