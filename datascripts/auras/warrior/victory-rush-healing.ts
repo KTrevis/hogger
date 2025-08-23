fe700db819b30ddcbb6984c17d62f486bc6f6d52
KTrevis <treviskevin77@gmail.com>
2025-08-23 16:10:38 +0200

    feat(victory-rush): fix + tooltip

--- datascripts/auras/warrior/victory-rush-healing.ts
+++ datascripts/auras/warrior/victory-rush-healing.ts
@@ -1,6 +1,8 @@
 import { std } from "wow/wotlk";
 import { INFINITE_DURATION, MODULE_NAME } from "../../utils/constants";
 import { HEALING_20_PCT } from "../../utils/auras/healing-20-percent";
+import { Spell } from "wow/wotlk/std/Spell/Spell";
+import { translate } from "../../utils/translation";
 
 const VICTORY_RUSH_SPELL = std.Spells.load(34428);
 
@@ -9,14 +11,12 @@ export const VICTORY_RUSH_HEALING = std.Spells.create(
   "victory-rush-healing"
 );
 
-VICTORY_RUSH_HEALING.Name.enGB
-  .set("Victory Rush Healing")
-  .Proc.mod((x) =>
-    x.SpellFamily.set(VICTORY_RUSH_SPELL.Family.get())
-      .ClassMask.B.set(VICTORY_RUSH_HEALING.ClassMask.B.get())
-      .PhaseMask.HIT.set(true)
-      .TriggerMask.DONE_SPELL_MELEE_DMG_CLASS.set(true)
-  )
+VICTORY_RUSH_HEALING.Proc.mod((x) =>
+  x.SpellFamily.set(VICTORY_RUSH_SPELL.Family.get())
+    .ClassMask.B.set(VICTORY_RUSH_SPELL.ClassMask.B.get())
+    .PhaseMask.HIT.set(true)
+    .TriggerMask.DONE_SPELL_MELEE_DMG_CLASS.set(true)
+)
   .Family.set(4)
   .ClassMask.set(0, 0, 0)
   .Duration.set(INFINITE_DURATION)
@@ -26,6 +26,17 @@ VICTORY_RUSH_HEALING.Name.enGB
       .TriggeredSpell.set(HEALING_20_PCT.ID)
       .ImplicitTargetA.UNIT_CASTER.set()
       .ImplicitTargetB.set(0)
-      .ChainTargets.set(0)
-      .ChainAmplitude.set(1)
   );
+
+namespace Translation {
+  export function english(spell: Spell) {
+    spell.Name.enGB.set("Victory Rush Healing");
+    spell.Description.enGB.set(
+      "Using Victory Rush heals you for 20% of your max health."
+    );
+  }
+}
+
+translate(VICTORY_RUSH_HEALING, {
+  enGB: Translation.english,
+});
