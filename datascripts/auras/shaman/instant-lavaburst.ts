f4d338937e39a29e557b853fcb11a5074b5db379
KTrevis <treviskevin77@gmail.com>
2025-08-23 16:36:56 +0200

    feat(instant-lavaburst): removed inheritance

--- datascripts/auras/shaman/instant-lavaburst.ts
+++ datascripts/auras/shaman/instant-lavaburst.ts
@@ -7,28 +7,33 @@ const LAVA_BURST = std.Spells.load(51505);
 
 const INSTANT_LAVABURST = std.Spells.create(
   MODULE_NAME,
-  "instant-lava-burst-aura",
-  57761
+  "instant-lava-burst-aura"
 );
 
-INSTANT_LAVABURST.Name.enGB
-  .set("Instant Lava Burst")
-  .Duration.setSimple(8 * 1000)
+INSTANT_LAVABURST.Duration.setSimple(8 * 1000)
   .Icon.set(LAVA_BURST.Icon.get())
-  .Attributes.IS_PASSIVE.set(true);
-
-INSTANT_LAVABURST.Family.set(LAVA_BURST.Family.get());
-INSTANT_LAVABURST.Effects.get(0).ClassMask.A.set(LAVA_BURST.ClassMask.A.get());
-INSTANT_LAVABURST.Effects.get(1).ClassMask.A.set(LAVA_BURST.ClassMask.A.get());
+  .Family.set(LAVA_BURST.Family.get())
+  .Effects.addMod((eff) =>
+    eff.Type.APPLY_AURA.set()
+      .ClassMask.B.set(LAVA_BURST.ClassMask.B.get())
+      .Aura.ADD_PCT_MODIFIER.set()
+      .Operation.CASTING_TIME.set()
+      .PercentBase.set(-999)
+      .ImplicitTargetA.UNIT_CASTER.set()
+  );
 
 namespace Translation {
   export function english(spell: Spell) {
-    const TOOLTIP = "Your Lava Burst is instant and does not cost mana.";
-    spell.AuraDescription.enGB.set(TOOLTIP);
-    spell.Description.enGB.set(TOOLTIP);
+    const TOOLTIP = "Your Lava Burst is instant.";
+    spell.AuraDescription.enGB
+      .set(TOOLTIP)
+      .Description.enGB.set(TOOLTIP)
+      .Name.enGB.set("Instant Lava Burst");
   }
 }
 
 translate(INSTANT_LAVABURST, {
   enGB: Translation.english,
 });
+
+console.log(INSTANT_LAVABURST.objectify());
